633a949ef8ef33d79e6f9d3c2763c9ab05124238
raghav151106 <raghavawasthi15116@gmail.com>
2025-06-12 19:18:14 +0530

    Raghav day1

---
+++ Day01_ProgrammingElements/CalculateAvg.java
@@ -0,0 +1,11 @@
+package Day01_ProgrammingElements;
+
+public class CalculateAvg {
+    public static void main(String[] args) {
+        int math=94;
+        int physics=95;
+        int chemistry=96;
+        int avg= (math + physics + chemistry)/3;
+        System.out.println("Sam’s average mark in PCM is "+ avg);
+    }
+}
