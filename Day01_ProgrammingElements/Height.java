44c66ea638c13f12cb42ac7db93e57c44cb4ff59
raghav151106 <raghavawasthi15116@gmail.com>
2025-06-13 09:40:07 +0530

    Raghav commit this

---
+++ Day01_ProgrammingElements/Height.java
@@ -0,0 +1,10 @@
+package Day01_ProgrammingElements;
+
+public class Height {
+    public static void main(String[] args) {
+        double heightinCentimeter = 500;
+        double inch = heightinCentimeter/2.54;
+        double feet = inch /12;
+        System.out.println("Your Height in cm is "+heightinCentimeter+" while in feet is "+ feet +" and inches is "+inch);
+    }
+}
--- Day01_ProgrammingElements/UniversityFee.java
+++ Day01_ProgrammingElements/UniversityFee.java
@@ -1,7 +1,13 @@
 package Day01_ProgrammingElements;
-
+import java.util.Scanner;
 public class UniversityFee {
     public static void main(String[] args) {
-        
+        Scanner sc =new Scanner (System.in);
+        int fee=sc.nextInt();
+        int discount= sc.nextInt();
+        int discountedAmount=(fee * discount)/100;
+        int discountedPrice= fee - discountedAmount;
+        System.out.println("The discount amount is INR "+discountedAmount+"and final discounted fee is INR "+discountedPrice);
+
     }
 }
