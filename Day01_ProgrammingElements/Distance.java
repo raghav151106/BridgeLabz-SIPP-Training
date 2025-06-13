08f8e36032d802d040638531c4c9c663a86606d7
raghav151106 <raghavawasthi15116@gmail.com>
2025-06-13 08:25:18 +0530

    Raghav DAY1

---
+++ Day01_ProgrammingElements/Distance.java
@@ -0,0 +1,9 @@
+package Day01_ProgrammingElements;
+
+public class Distance {
+    public static void main(String[] args) {
+        float distance=10.8f;
+        float miles=1.6f * distance;
+        System.out.println("The distance "+distance+"km in miles is "+miles);
+    }
+}
