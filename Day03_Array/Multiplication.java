7b55a17e4c486c2e883a1612b329e2bc77663af4
raghav151106 <raghavawasthi15116@gmail.com>
2025-06-17 10:02:46 +0530

    Raghav add multiplication table of number

---
+++ Day03_Array/Multiplication.java
@@ -0,0 +1,15 @@
+package Day03_Array;
+
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            int a= i*n;
+            System.out.print(a+" ");
+        }
+    }
+
+}
